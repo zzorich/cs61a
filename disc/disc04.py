8ec6f48eb212cba56733557846befd4c4b0f2f81
lingji zhou <lingji.zhou17@gmail.com>
2022-01-01 21:35:07 +0000

    Correct the name.

---
+++ disc/disc04.py
@@ -0,0 +1,60 @@
+def count_stair_ways(n):
+    """Returns the number of ways to climb up a flight of
+    n stairs, moving either 1 step or 2 steps at a time.
+    >>> count_stair_ways(4)
+    5
+    """
+    "*** YOUR CODE HERE ***"
+    if n<0:
+        return 0
+    if n == 0:
+        return 1
+    
+    return count_stair_ways(n-1)+count_stair_ways(n-2)
+
+def count_k(n, k):
+    """ Counts the number of paths up a flight of n stairs
+    when taking up to and including k steps at a time.
+    >>> count_k(3, 3) # 3, 2 + 1, 1 + 2, 1 + 1 + 1
+    4
+    >>> count_k(4, 4)
+    8
+    >>> count_k(10, 3)
+    274
+    >>> count_k(300, 1) # Only one step at a time
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    if n<0:
+        return 0
+    if n == 0:
+        return 1
+    
+    count_ways = 0
+    for i in range(1,k+1):
+        count_ways += count_k(n-i, k)
+    
+    return count_ways
+
+def even_weighted(s):
+    """
+    >>> x = [1, 2, 3, 4, 5, 6]
+    >>> even_weighted(x)
+    [0, 6, 20]
+    """
+    return [s[i]*i for i in range(len(s)) if i%2 ==0 ]
+
+def max_product(s):
+    """Return the maximum product that can be formed using
+    non-consecutive elements of s.
+    >>> max_product([10,3,1,9,2]) # 10 * 9
+    90
+    >>> max_product([5,10,5,10,5]) # 5 * 5 * 5
+    125
+    >>> max_product([])
+    1
+    """
+    if s == []:
+        return 1
+
+    return max(s[0]*max_product(s[2:]), max_product(s[1:]))
