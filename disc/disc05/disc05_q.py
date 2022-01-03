d3466fa88648d4e26a25baa3037584cfd95ac2b3
lingji zhou <lingji.zhou17@gmail.com>
2022-01-03 21:34:07 +0000

    Add disc05.

---
+++ disc/disc05/disc05_q.py
@@ -0,0 +1,236 @@
+
+#Q2
+def height(t):
+    """Return the height of a tree.
+
+    >>> t = tree(3, [tree(5, [tree(1)]), tree(2)])
+    >>> height(t)
+    2
+    >>> t = tree(3, [tree(1), tree(2, [tree(5, [tree(6)]), tree(1)])])
+    >>> height(t)
+    3
+    """
+    "*** YOUR CODE HERE ***"
+    if is_leaf(t):
+        return 0
+
+    return 1 + max([height(b) for b in branches(t)])
+
+#Q3
+def max_path_sum(t):
+    """Return the maximum path sum of the tree.
+
+    >>> t = tree(1, [tree(5, [tree(1), tree(3)]), tree(10)])
+    >>> max_path_sum(t)
+    11
+    """
+    "*** YOUR CODE HERE ***"
+    if is_leaf(t):
+        return label(t)
+
+    return label(t) + max([max_path_sum(b) for b in branches(t)])
+
+#Q4
+def find_path(t, x):
+    """
+    >>> t = tree(2, [tree(7, [tree(3), tree(6, [tree(5), tree(11)])] ), tree(15)])
+    >>> find_path(t, 5)
+    [2, 7, 6, 5]
+    >>> find_path(t, 10)  # returns None
+    """
+    if label(t) == x:
+        return [x]
+    for b in branches(t):
+        path = [label(t)]
+        if find_path(b, x):
+            return path + find_path(b, x)
+
+#Q5
+def my_map(fn, seq):
+    """Applies fn onto each element in seq and returns a list.
+    >>> my_map(lambda x: x*x, [1, 2, 3])
+    [1, 4, 9]
+    """
+    "*** YOUR CODE HERE ***"
+    return [fn(i) for i in seq]
+    
+#Q6
+def my_filter(pred, seq):
+    """Keeps elements in seq only if they satisfy pred.
+    >>> my_filter(lambda x: x % 2 == 0, [1, 2, 3, 4])  # new list has only even-valued elements
+    [2, 4]
+    """
+    "*** YOUR CODE HERE ***"
+    return [i for i in seq if pred(i)]
+
+#Q7
+def my_reduce(combiner, seq):
+    """Combines elements in seq using combiner.
+    seq will have at least one element.
+    >>> my_reduce(lambda x, y: x + y, [1, 2, 3, 4])  # 1 + 2 + 3 + 4
+    10
+    >>> my_reduce(lambda x, y: x * y, [1, 2, 3, 4])  # 1 * 2 * 3 * 4
+    24
+    >>> my_reduce(lambda x, y: x * y, [4])
+    4
+    >>> my_reduce(lambda x, y: x + 2 * y, [1, 2, 3]) # (1 + 2 * 2) + 2 * 3
+    11
+    """
+    "*** YOUR CODE HERE ***"
+    if len(seq) == 1:
+        return seq[0]
+    
+    return combiner(my_reduce(combiner, seq[:-1]), seq[-1])
+
+#Q8
+def count_palindromes(L):
+    """The number of palindromic words in the sequence of strings
+    L (ignoring case).
+
+    >>> count_palindromes(("Acme", "Madam", "Pivot", "Pip"))
+    2
+    """
+    return my_reduce(lambda x,y: x+y, my_map(lambda s: 1, my_filter(lambda s: s[::-1].lower() == s.lower(), L)))
+
+
+#Q9
+def sum_tree(t):
+    """
+    Add all elements in a tree.
+    >>> t = tree(4, [tree(2, [tree(3)]), tree(6)])
+    >>> sum_tree(t)
+    15
+    """
+    "*** YOUR CODE HERE ***"
+    return sum([sum_tree(b) for b in branches(t)], start = label(t))
+
+#Q10
+def balanced(t):
+    """
+    Checks if each branch has same sum of all elements and
+    if each branch is balanced.
+    >>> t = tree(1, [tree(3), tree(1, [tree(2)]), tree(1, [tree(1), tree(1)])])
+    >>> balanced(t)
+    True
+    >>> t = tree(1, [t, tree(1)])
+    >>> balanced(t)
+    False
+    >>> t = tree(1, [tree(4), tree(1, [tree(2), tree(1)]), tree(1, [tree(3)])])
+    >>> balanced(t)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    sum_branches = [sum_tree(b) for b in branches(t)]
+    return all([balanced(b) for b in branches(t)]) and all([i == sum_branches[0] for i in sum_branches[1:]])    #Could be in one line ,replace sum_branches with above expression
+
+Q11
+def hailstone_tree(n, h):
+    """Generates a tree of hailstone numbers that will reach N, with height H.
+    >>> print_tree(hailstone_tree(1, 0))
+    1
+    >>> print_tree(hailstone_tree(1, 4))
+    1
+        2
+            4
+                8
+                    16
+    >>> print_tree(hailstone_tree(8, 3))
+    8
+        16
+            32
+                64
+            5
+                10
+    """
+    if h == 0:
+        return tree(n)
+    branches = [hailstone_tree(2*n, h-1)]
+    if (n-1)%3 == 0 and ((n-1)//3)%2 == 1 and ((n-1)//3)!=1:
+        branches += [hailstone_tree((n-1)//3, h-1)]
+    return tree(n, branches)
+
+def print_tree(t):
+    def helper(i, t):
+        print("    " * i + str(label(t)))
+        for b in branches(t):
+            helper(i + 1, b)
+    helper(0, t)
+
+
+
+
+
+
+
+
+
+# Tree ADT
+
+def tree(label, branches=[]):
+    """Construct a tree with the given label value and a list of branches."""
+    for branch in branches:
+        assert is_tree(branch), 'branches must be trees'
+    return [label] + list(branches)
+
+
+def label(tree):
+    """Return the label value of a tree."""
+    return tree[0]
+
+
+def branches(tree):
+    """Return the list of branches of the given tree."""
+    return tree[1:]
+
+
+def is_tree(tree):
+    """Returns True if the given tree is a tree, and False otherwise."""
+    if type(tree) != list or len(tree) < 1:
+        return False
+    for branch in branches(tree):
+        if not is_tree(branch):
+            return False
+    return True
+
+
+def is_leaf(tree):
+    """Returns True if the given tree's list of branches is empty, and False
+    otherwise.
+    """
+    return not branches(tree)
+
+
+# def print_tree(t, indent=0):
+#     """Print a representation of this tree in which each node is
+#     indented by two spaces times its depth from the root.
+
+#     >>> print_tree(tree(1))
+#     1
+#     >>> print_tree(tree(1, [tree(2)]))
+#     1
+#       2
+#     >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
+#     >>> print_tree(numbers)
+#     1
+#       2
+#       3
+#         4
+#         5
+#       6
+#         7
+#     """
+#     print('  ' * indent + str(label(t)))
+#     for b in branches(t):
+#         print_tree(b, indent + 1)
+
+
+def copy_tree(t):
+    """Returns a copy of t. Only for testing purposes.
+
+    >>> t = tree(5)
+    >>> copy = copy_tree(t)
+    >>> t = tree(6)
+    >>> print_tree(copy)
+    5
+    """
+    return tree(label(t), [copy_tree(b) for b in branches(t)])
