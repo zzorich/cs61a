2c7a8ac84a267cc96c26eacea84afb8a0559d9e6
lingji zhou <lingji.zhou17@gmail.com>
2022-01-12 23:01:04 +0000

    lab09 update

---
+++ disc/disc08.py
@@ -0,0 +1,140 @@
+class Link:
+    """A linked list."""
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
+
+def sum_nums(s):
+    """
+    >>> a = Link(1, Link(6, Link(7)))
+    >>> sum_nums(a)
+    14
+    """
+    "*** YOUR CODE HERE ***"
+    if s is Link.empty:
+        return 0
+
+    return s.first + sum_nums(s.rest)
+
+
+def multiply_lnks(lst_of_lnks):
+    """
+    >>> a = Link(2, Link(3, Link(5)))
+    >>> b = Link(6, Link(4, Link(2)))
+    >>> c = Link(4, Link(1, Link(0, Link(2))))
+    >>> p = multiply_lnks([a, b, c])
+    >>> p.first
+    48
+    >>> p.rest.first
+    12
+    >>> p.rest.rest.rest is Link.empty
+    True
+    """
+    product = 1
+    for lnk in lst_of_lnks:
+        if lnk is Link.empty:
+            return Link.empty
+        product *= lnk.first
+    
+    return Link(product, multiply_lnks([link.rest for link in lst_of_lnks]))
+
+def flip_two(s):
+    """
+    >>> one_lnk = Link(1)
+    >>> flip_two(one_lnk)
+    >>> one_lnk
+    Link(1)
+    >>> lnk = Link(1, Link(2, Link(3, Link(4, Link(5)))))
+    >>> flip_two(lnk)
+    >>> lnk
+    Link(2, Link(1, Link(4, Link(3, Link(5)))))
+    """
+    "*** YOUR CODE HERE ***"
+
+    # For an extra challenge, try writing out an iterative approach as well below!
+    "*** YOUR CODE HERE ***"
+    if s is Link.empty or s.rest is Link.empty:
+        return 
+    s.first, s.rest.first = s.rest.first, s.first
+    flip_two(s.rest.rest)
+
+def make_even(t):
+    """
+    >>> t = Tree(1, [Tree(2, [Tree(3)]), Tree(4), Tree(5)])
+    >>> make_even(t)
+    >>> t.label
+    2
+    >>> t.branches[0].branches[0].label
+    4
+    """
+    "*** YOUR CODE HERE ***"
+    if t.label % 2 == 1:
+        t.label += 1
+    
+    for subtree in t.branches:
+        make_even(subtree)
+
+def leaves(t):
+    """Returns a list of all the labels of the leaf nodes of the Tree t.
+
+    >>> leaves(Tree(1))
+    [1]
+    >>> leaves(Tree(1, [Tree(2, [Tree(3)]), Tree(4)]))
+    [3, 4]
+    """
+    "*** YOUR CODE HERE ***"
+    lves = []
+    if t.is_leaf():
+        lves.append(t.label)
+    
+    for subtree in t.branches:
+        lves += leaves(subtree)
+    
+    return lves
+
+def find_paths(t, entry):
+    """
+    >>> tree_ex = Tree(2, [Tree(7, [Tree(3), Tree(6, [Tree(5), Tree(11)])]), Tree(1, [Tree(5)])])
+    >>> find_paths(tree_ex, 5)
+    [[2, 7, 6, 5], [2, 1, 5]]
+    >>> find_paths(tree_ex, 12)
+    []
+    """
+
+    paths = []
+    if t.label == entry:
+        paths.append([t.label])
+
+    for subtree in t.branches:
+        for path in find_paths(subtree, entry):
+            paths.append([t.label] + path)
+    
+    return paths
+
+
+class Tree:
+    def __init__(self, label, branches=[]):
+        for b in branches:
+            assert isinstance(b, Tree)
+        self.label = label
+        self.branches = branches
+
+    def is_leaf(self):
+        return not self.branches
